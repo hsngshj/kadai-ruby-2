5256d3ba21f353a5f5770307d186ddf1d51d752a
hsngshj <standbyme1212@gmail.com>
2022-11-26 13:47:46 +0000

    繰り返し分をメソッドの外に記述

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,23 +1,25 @@
 def fizzubuzz(num)
-  num_max = 100
-
-  while num <= num_max
-    if num % 3 == 0 
-      if num % 5 == 0
-        puts 'FizzBuzz'
-        num += 1
-      else
-        puts 'Fizz'
-        num += 1
-      end
-    elsif num % 5 == 0
-      puts 'Buzz'
+  if num % 3 == 0 
+    if num % 5 == 0
+      return 'FizzBuzz'
       num += 1
     else
-      puts num
+      return 'Fizz'
       num += 1
     end
+  elsif num % 5 == 0
+    return 'Buzz'
+    num += 1
+  else
+    return num
+    num += 1
   end
 end
 
-puts fizzubuzz(1)
+num = 1
+num_max = 100
+
+while num <= num_max
+  puts fizzubuzz(num)
+  num += 1
+end
